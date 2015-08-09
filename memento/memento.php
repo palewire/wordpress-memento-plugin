d09b05d2a12a7e2a7475cb5afff53d57dafed399
palewire <ben.welsh@gmail.com>
2015-08-09 16:25:16 -0700

    Plugin and license headers added to the main file

--- memento/memento.php
+++ memento/memento.php
@@ -1,1 +1,35 @@
+<?php
+/*
+Plugin Name: Memento
+Plugin URI:  http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A plugin for Wordpress web sites to enable the Memento framework for time-based access
+Version:     0.0.1
+Author:      Ben Welsh
+Author URI:  http://palewi.re/who-is-ben-welsh/
+License:     MIT
+License URI: http://opensource.org/licenses/MIT
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Ben Welsh
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
