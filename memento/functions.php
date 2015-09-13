f9fcf77e0f7db970b35d455eefe7a717d871dc99
palewire <ben.welsh@gmail.com>
2015-09-12 20:19:28 -0700

    Added functions to get the max and min post dates for the timemap as well

--- memento/functions.php
+++ memento/functions.php
@@ -22,7 +22,30 @@ function get_post_revisions($post_id)
  * Returns a the timegate URL where the previous version of the
  * post is hosted.
  */
-function get_revision_permalink($post, $revision) {
+function get_revision_permalink($post, $revision)
+{
     $post_url = get_post_permalink($post);
     return $post_url . '&revision=' . $revision->ID;
+}
+
+/**
+ * Returns the maximum post_date_gmt from a list of posts or revisions
+ */
+function get_max_post_date_gmt($revision_list)
+{
+    $datetimes = array_map(function($revision) {
+        return $revision->post_date_gmt;
+    }, $revision_list);
+    return max($datetimes);
+}
+
+/**
+ * Returns the minimum post_date_gmt from a list of posts or revisions
+ */
+function get_min_post_date_gmt($revision_list)
+{
+    $datetimes = array_map(function($revision) {
+        return $revision->post_date_gmt;
+    }, $revision_list);
+    return min($datetimes);
 }
--- memento/timemap-list.php
+++ memento/timemap-list.php
@@ -6,8 +6,8 @@
 <<?php echo $timemap_url ?>>;rel="original",
  <<?php echo get_timemap_list_permalink($timemap_url) ?>>
    ; rel="self";type="application/link-format"
-   ; from="{{ minimum_datetime|httpdate }}"
-   ; until="{{ maximum_datetime|httpdate }}",
+   ; from="<?php echo get_min_post_date_gmt($revision_list) ?> GMT"
+   ; until="<?php echo get_max_post_date_gmt($revision_list) ?> GMT",
 <?php $i=0; $len=count($revision_list); foreach($revision_list as $revision) :?>
    <<?php echo get_revision_permalink($post, $revision) ?>>
    ; rel="<?php if ($i == 0) echo 'first '; if ($i == $len-1) echo 'last ' ?>memento"; datetime="<?php echo $revision->post_date_gmt ?> GMT"<?php if ($i != $len-1) echo ",\r\n" ?>
